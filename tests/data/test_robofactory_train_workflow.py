043464aeaacf529f5ebdd3df830797aba7a956e1
XianzheFan <xianzhef@nvidia.com>
2026-06-21 10:31:59 +0800

    Pin loss-balanced Gamma code cache

--- tests/data/test_robofactory_train_workflow.py
+++ tests/data/test_robofactory_train_workflow.py
@@ -6,8 +6,8 @@ import yaml
 REPO_ROOT = Path(__file__).resolve().parents[2]
 WORKFLOW_PATH = REPO_ROOT / "osmo_workflows/robofactory/train_liftbarrier_shared_global.yaml"
 STAGED_WORKFLOW_PATH = REPO_ROOT / "osmo_workflows/robofactory/train_liftbarrier_gamma_staged.yaml"
-CODE_CACHE_URI = "swift://pdx.s8k.io/AUTH_team-gear/datasets/users/xianzhef/oci-migration/dreamzero_code_gamma_dense_teacher_20260621"
-EXPECTED_CODE_COMMIT = "69508a3588c10be56d66c9ba7c2d308413001194"
+CODE_CACHE_URI = "swift://pdx.s8k.io/AUTH_team-gear/datasets/users/xianzhef/oci-migration/dreamzero_code_gamma_lossbalance_20260621"
+EXPECTED_CODE_COMMIT = "81a4a5df79aa5dcdd5ae50ef02a89f219a740ee8"
 
 
 def _task_by_name(workflow, name):
